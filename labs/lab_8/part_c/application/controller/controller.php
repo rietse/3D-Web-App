49a8f16db1faac87952b06b93bbdb7de95ff71c9
rietse <rietse68@gmail.com>
2023-05-08 17:54:11 +0100

    lab 8

---
+++ labs/lab_8/part_c/application/controller/controller.php
@@ -0,0 +1,75 @@
+<?php
+// Create the controller class for the MVC design pattern
+class Controller {
+
+	// Declare public variables for the controller class
+	public $load;
+	public $model;
+
+	// Create functions for the controller class
+	function __construct($pageURI = null) // constructor of the class
+	{
+		$this->load = new Load();
+		$this->model = new Model();
+		// determine what page you are on
+		$this->$pageURI();
+	}
+    // home page function
+	function home()
+	{
+		$data = $this->model->model3D_info();
+		$this->load->view('view3DAppTest_2', $data);
+	}
+
+	function apiCreateTable()
+	{
+	  	// echo "Create table function";
+		$data = $this->model->dbCreateTable();
+		$this->load->view('viewMessage', $data);
+	}
+	function apiInsertData()
+	{
+		$data = $this->model->dbInsertData();
+	   	$this->load->view('viewMessage', $data);
+	}
+	function apiGetData()
+	{
+		$data = $this->model->dbGetData();
+		$this->load->view('view3DAppData', $data);
+	}
+
+	function apiGetFlickrService()
+    	{
+    		$this->load->view('viewFlickrService');
+    	}
+
+    function apiGetJson()
+    {
+    		$this->load->view('viewJson');
+    }
+
+    function apiLoadImage()
+    {
+    	   $data = $this->model->dbGetBrand();
+    	   // Note, the viewDrinks.php view being loaded here calls a new model
+    	   // called modelDrinkDetails.php, which is not part of the Model class
+    	   // It is a separate model illustrating that you can have many models
+    	   $this->load->view('viewDrinks', $data);
+    }
+	function dbCreateTable()
+	{
+		echo "Create Table Function";
+	}
+
+	function dbInsertData()
+	{
+		echo "Data Insert Function";
+	}
+
+	function dbGetData()
+	{
+		echo "Data Read Function";
+	}
+
+}
+?>
